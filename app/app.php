e19adeccf398b701487798c51b8081126787b2fc
HK Kahng and Jon Toler <jon@>
2016-09-28 09:05:06 -0700

    create initial files and directories

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Students.php";
+    require_once __DIR__."/../src/Courses.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=bibliotheque';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+    $app->get("/", function () use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
+
+    }
+?>
